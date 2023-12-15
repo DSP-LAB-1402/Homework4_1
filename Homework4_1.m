01df598082a06616065164e5e7be7a266928a343
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-12-15 17:25:36 +0330

    Finish 4.2.4

--- Homework4_1.m
+++ Homework4_1.m
@@ -60,4 +60,17 @@ subplot(1, 3, 3)
 imshow(filtered);
 title('Filtered Image');
 
+%% 4.2.4
+kernel = ones(5, 5) / 25;
+filtered = imfilter(noisy, kernel);
 
+figure('Name', "Filtered Image");
+subplot(1, 3, 1)
+imshow(img2);
+title('Original Image');
+subplot(1, 3, 2)
+imshow(noisy);
+title('Noisy Image');
+subplot(1, 3, 3)
+imshow(filtered);
+title('Filtered Image');
